4a5eca7dbf99981a11666634e8adebc9ba608df3
arlindnimanii <146958254+arlindnimanii@users.noreply.github.com>
2024-03-12 23:01:08 +0100

    Add files via upload

---
+++ database/migrations/2023_04_05_191742_create_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('allow_friend_requests')->default(true);
+            $table->integer('nr_posts_in_homepage')->default(12);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
